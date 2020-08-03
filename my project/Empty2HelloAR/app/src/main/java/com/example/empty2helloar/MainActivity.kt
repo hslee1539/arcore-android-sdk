3bc9683f054d6cc9917017998260b92cb981d1cc
leehs <leehs@rsupport.com>
2020-08-03 14:03:19 +0900

    surface 세팅

--- my project/Empty2HelloAR/app/src/main/java/com/example/empty2helloar/MainActivity.kt
+++ my project/Empty2HelloAR/app/src/main/java/com/example/empty2helloar/MainActivity.kt
@@ -1,18 +1,47 @@
 package com.example.empty2helloar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,GLSurfaceView.Renderer{
+    private var viewportWidth : Int = 0
+    private var viewportHeight : Int = 0
+    private var viewportChange : Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Set up renderer.
+        surfaceview.apply{
+            preserveEGLContextOnPause = true
+            setEGLContextClientVersion(2)
+            setEGLConfigChooser(8,8,8,8,16,0)
+            setRenderer(this@MainActivity)
+            renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+            setWillNotDraw(false)
+        }
+
     }
 
     override fun onResume() {
         super.onResume()
+        surfaceview.onResume()
         if(!PermissionHelper.hasCameraPermission(this))
             PermissionHelper.requestCameraPermission(this)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        surfaceview.onPause()
     }
 
     override fun onRequestPermissionsResult(
@@ -24,5 +53,20 @@ class MainActivity : AppCompatActivity() {
         if(!PermissionHelper.hasCameraPermission(this))
             if(!PermissionHelper.shouldShowRequestPermissionRationale(this))
                 PermissionHelper.launchPermissionSettings(this)
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        this.viewportWidth = width
+        this.viewportHeight = height
+        this.viewportChange = true
+    }
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f)
+        //TODO("Not yet implemented")
     }
 }
